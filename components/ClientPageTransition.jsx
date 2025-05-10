736c0b61d8d62157648a0ba0c67cd905b54dd5cd
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2025-05-10 14:54:06 +0530

    style: Format dynamic import for PageTransitionWrapper for improved readability

--- components/ClientPageTransition.jsx
+++ components/ClientPageTransition.jsx
@@ -4,9 +4,12 @@ import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
 // Dynamically import PageTransition with ssr: false
-const PageTransitionWrapper = dynamic(() => import("@/components/PageTransitionWrapper"), {
-  ssr: false,
-});
+const PageTransitionWrapper = dynamic(
+  () => import("@/components/PageTransitionWrapper"),
+  {
+    ssr: false,
+  }
+);
 
 export default function ClientPageTransition() {
   return (
